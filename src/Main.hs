b3f6f0dfe71fabe1ee9a0d227ce07d8cfc3b9357
Daniel Corson <danl@alum.mit.edu>
2009-05-03 21:37:16 -0700

    use guid's to make conflict resolution sensible

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,13 @@
 module Main where
 
-import Data.Graph.Inductive.Graph  -- need?
-import Data.Graph.Inductive.Tree
+import Control.Applicative hiding (empty)
+import Control.Arrow
+import Control.Monad.Random
+import Data.Graph.Inductive
+import Data.List
 import Data.Maybe
 import FUtil
+import Numeric
 import System.Console.GetOpt
 import qualified Data.Set as S
 
@@ -17,41 +21,78 @@ options :: [OptDescr (Options -> Options)]
 options = [
   ]
 
-data TaskNode = TaskNode {
+data Guid = Guid Integer
+  deriving (Eq, Ord)
+
+instance Show Guid where
+  show (Guid i) = (\ s -> replicate (32 - length s) '0' ++ s) (showHex i "")
+
+instance Read Guid where
+  readsPrec _ = map (first Guid) . readHex
+
+data Task = Task {
   taskName :: String,
-  taskDesc :: String
+  taskDesc :: String,
+  taskGuid :: Guid
   }
+  deriving (Eq, Ord, Read, Show)
 -- KwlrThan influences ordering in default view
 -- (todo)
-data Conn = DepsOn | Contains | HasProp | KwlrThan deriving (Eq, Ord, Show)
-type Tasks = Gr TaskNode (S.Set Conn)
+data Conn = DepsOn | Contains | HasProp | KwlrThan
+  deriving (Eq, Ord, Read, Show)
+
+type Tasks = Gr Task (S.Set Conn)
+type SRand = Rand StdGen
 
 data TaskLogAction =
-  AddTask TaskNode |
-  DelTask TaskNode |
-  AddConn TaskNode TaskNode Conn |
-  DelConn TaskNode TaskNode Conn
+  AddTask Guid Task |
+  DelTask Guid |
+  AddConn Guid Guid Conn |
+  DelConn Guid Guid Conn
 
 tasksFromLog :: [TaskLogAction] -> Tasks
-tasksFromLog = foldr tasksAddLogAction empty
+tasksFromLog = foldl' (flip tasksDoLogAction) tasksEmpty
 
 tasksDoLogAction :: TaskLogAction -> Tasks -> Tasks
-tasksDoLogAction action (AddTask node) = error "todo"
-tasksDoLogAction action (DelTask node) = error "todo"
-tasksDoLogAction action (AddConn node1 node2 conn) = error "todo"
-tasksDoLogAction action (DelConn node1 node2 conn) = error "todo"
+tasksDoLogAction (AddTask guid task) = addTask guid task
+tasksDoLogAction (DelTask guid) = delTask guid
+tasksDoLogAction (AddConn guid1 guid2 conn) = addConn guid1 guid2 conn
+tasksDoLogAction (DelConn guid1 guid2 conn) = delConn guid1 guid2 conn
 
-tasksEmpty :: Gr [Char] b
-ltasksEmpty = mkGraph [(0, "top")] []
+tasksEmpty :: Tasks
+tasksEmpty = mkGraph [(0, Task "top" "" $ Guid 0)] []
 
-addTo :: Node -> TNode -> Tasks -> (Node, Tasks)
-addTo pI t g = (i, insEdge (pI, i, S.singleton Contains) $ insNode (i, t) g)      where
-  [i] = newNodes 1 g
+addTask :: Guid -> Task -> Tasks -> Tasks
+addTask guid task tasks =
+  insEdge (findNode guid tasks, i, S.singleton Contains) $
+  insNode (i, task) tasks
+  where
+  [i] = newNodes 1 tasks
 
-pretty :: (Graph gr) => Node -> gr [Char] (S.Set Conn) -> String
-pretty i g = unlines .
+delTask :: Guid -> Tasks -> Tasks
+delTask guid tasks = delNode (findNode guid tasks) tasks
+
+addConn guid1 guid2 conn tasks =
+  insEdge (findNode guid1 tasks, findNode guid2 tasks,
+  S.insert conn $ findEdge guid1 guid2 tasks) tasks
+
+delConn guid1 guid2 conn tasks =
+  insEdge (findNode guid1 tasks, findNode guid2 tasks,
+  S.delete conn $ findEdge guid1 guid2 tasks) tasks
+
+findNode guid tasks = node where
+  node:_ = map fst . filter ((== guid) . taskGuid . snd) $ labNodes tasks
+
+findEdge :: Guid -> Guid -> Tasks -> S.Set Conn
+findEdge guid1 guid2 tasks = maybe S.empty (\ (n1, n2, c) -> c) . listToMaybe .
+  filter (\ (n1, n2, c) -> n1 == findNode guid1 tasks &&
+                           n2 == findNode guid2 tasks) $ labEdges tasks
+
+pretty :: Guid -> Tasks -> String
+pretty guid g = unlines .
   zipWith (\ n l -> padl '0' 2 (show n) ++ " " ++ l) [0..] $ prettyInd 0 i
   where
+  i = findNode guid g
   prettyInd ind i =
     -- todo:
     --    80 cols
@@ -59,17 +100,39 @@ pretty i g = unlines .
     --    hide done
     concatMap (\ (x, conns) -> map (indOf ind ++) $
       if Contains `S.member` conns
-        then [fromJust (lab g x)] ++ prettyInd (ind + 1) x
+        then [taskName . fromJust $ lab g x] ++ prettyInd (ind + 1) x
         else []
       ) $ lsuc g i
     where
     indOf ind = if ind == 0 then "" else replicate (2 * (ind - 1)) ' ' ++ "- "
 
+newGuid :: SRand Guid
+newGuid = Guid <$> getRandomR (0, 2 ^ 128)
+
+newTask :: String -> String -> SRand Task
+newTask name desc = Task name desc <$> newGuid
+
+save :: (Graph gr) => gr Task (S.Set Conn) -> String
+save tasks = nodesStr ++ edgesStr where
+  nodesStr = unlines . sort $ map showNode nodes
+  edgesStr = unlines . sort $ map showEdge edges
+  showNode (_, Task name desc guid) =
+    show guid ++ " " ++ show name ++ " " ++ show desc
+  showEdge (x, y, c) =
+    show (fromJust $ lookup x nodeGuids) ++ " " ++
+    show (fromJust $ lookup y nodeGuids) ++ " " ++ show c
+  nodeGuids = map (second taskGuid) nodes
+  nodes = labNodes tasks
+  edges = labEdges tasks
+
 main :: IO ()
 main = do
+  t1 <- evalRandIO $ newTask "projects" ""
+  t2 <- evalRandIO $ newTask "programming" ""
   let
-    (n, tasks) = addTo 0 "projects" tasksEmpty
-    (n', tasks') = addTo n "programming" tasks
-  (opts, args) <- doArgs "" defOpts options
-  clrScr
-  putStr $ take 100 $ pretty 0 tasks'
+    tasks = tasksFromLog [
+      AddTask (Guid 0) t1,
+      AddTask (taskGuid t1) t2]
+  putStrLn $ pretty (Guid 0) tasks
+  putStrLn $ pretty (taskGuid t1) tasks
+  putStrLn $ save tasks
